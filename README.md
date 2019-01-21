44f83f5d2e39da4900c243298e37ca0bde03e7d9
kirk <shirt@worm-2.local>
2019-01-20 21:14:50 -0500

    readme and setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pytda
+
+This package makes extensive use of the python bindings for the Dionysus C++ library (http://mrzv.org/software/dionysus2).
+In order to install Dionysus you will need
+    - CMake
+    - GCC >= 5.4
+    - Boost 1.55.
+
+To build the package and install all requirements run the following from the project's root directory
+
+    python setup.py -r build
+
+A modified version of the PyDEC library (https://github.com/hirani/pydec) is included as it is currently not maintained.
--- setup.py
+++ setup.py
@@ -1,45 +1,12 @@
-# import setuptools
-#
-# setuptools.setup(
-#     name="pytda",
-#     version="0.0.1",
-#     author="Kirk Gardner",
-#     author_email="kirk.gardner@uconn.edu",
-#     description="Applications of Topological Data Analysis in python.",
-#     long_description="An exploration of homological sensor networks, "\
-#                     "circular coordinates, and discrete exterior calculus "\
-#                     "in python using Dionysus and pydec.",
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/shirtd/AFRL_TDA",
-#     packages=setuptools.find_packages(),
-#     install_requires=[
-#         "argparse",
-#         "cython",
-#         "numpy",
-#         "scipy",
-#         "matplotlib",
-#         "dionysus", # requires cmake, boost
-#         "scikit-image",
-#         "shapely"
-#     ]
-# )
-
 import os, sys
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-            assume_default_configuration=True,
-            delegate_options_to_subpackages=True,
-            quiet=True)
-
+    config.set_options(ignore_setup_xxx_py=True, assume_default_configuration=True,
+                        delegate_options_to_subpackages=True, quiet=True)
     config.add_subpackage('pydec')
     config.add_subpackage('tda')
-    # config.add_data_files(('pydec','*.txt'))
-
-    # config.get_version(os.path.join('pydec','version.py')) # sets config.version
-
     return config
 
 def setup_package():
@@ -49,27 +16,38 @@ def setup_package():
     old_path = os.getcwd()
     local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
     os.chdir(local_path)
-    sys.path.insert(0,local_path)
-    sys.path.insert(0,os.path.join(local_path, 'pydec')) # to retrive version
-    sys.path.insert(0,os.path.join(local_path, 'tda')) # to retrive version
+    sys.path.insert(0, local_path)
+    sys.path.insert(0, os.path.join(local_path, 'pydec')) # to retrive version
+    sys.path.insert(0, os.path.join(local_path, 'tda')) # to retrive version
 
     try:
         setup(
-            name = 'pydec',
-            maintainer = "PyDEC Developers",
-            maintainer_email = "wnbell@gmail.com",
-            # description = DOCLINES[0],
-            # long_description = "\n".join(DOCLINES[2:]),
-            url = "http://www.graphics.cs.uiuc.edu/~wnbell/",
-            download_url = "http://code.google.com/p/pydec/downloads/list",
-            license = 'BSD',
-            # classifiers=filter(None, CLASSIFIERS.split('\n')),
-            platforms = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+            name="pytda",
+            version="0.0.1",
+            author="Kirk Gardner",
+            author_email="kirk.gardner@uconn.edu",
+            description="Applications of Topological Data Analysis in python.",
+            long_description="An exploration of homological sensor networks, "\
+                            "circular coordinates, and discrete exterior calculus "\
+                            "in python using Dionysus and pydec.",
+            long_description_content_type="text/markdown",
+            url="https://github.com/shirtd/pytda",
+            # packages=setuptools.find_packages(),
+            install_requires=[
+                "argparse",
+                "cython",
+                "numpy",
+                "scipy",
+                "matplotlib",
+                "dionysus", # requires cmake, boost
+                "scikit-image",
+                "shapely"
+            ],
             configuration=configuration)
+        )
     finally:
         del sys.path[0]
         os.chdir(old_path)
-
     return
 
 if __name__ == '__main__':
